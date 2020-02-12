b8c7ab37e5ad99fc7c3975a849746c7af48afa31
hjtan <43205976+hjtan516@users.noreply.github.com>
2020-02-12 19:48:18 +0800

    Update consine.m

--- Code/consine.m
+++ Code/consine.m
@@ -6,7 +6,10 @@ function [ kd,kl ] = consine( interaction )
     kd = zeros(nd);
     for i = 1:nd
         for j = 1:nd
-            kd(i,j) =interaction(i,:)*(interaction(j,:))'/(norm(interaction(i,:))*norm(interaction(j,:)));
+            denomi = norm(interaction(i, :)) * norm(interaction(j, :));
+            if denomi~=0
+                kd(i,j) =interaction(i,:)*(interaction(j,:))'/denomi;
+            end
         end
     end
 
@@ -14,7 +17,10 @@ function [ kd,kl ] = consine( interaction )
     kl = zeros(nl);
     for i = 1:nl
         for j = 1:nl
-            kl(i,j) =(interaction(:,i))'*interaction(:,j)/(norm(interaction(:,i))*norm(interaction(:,j)));
+            denomi = norm(interaction(:,i))*norm(interaction(:,j));
+            if denomi~=0
+                kl(i,j) =(interaction(:,i))'*interaction(:,j)/denomi;
+            end
         end
     end
 end
